d24f5bd2d37fe8dad4cb025b0554e67871a85756
tututu1234 <15268159190@163.com>
2017-06-15 15:22:18 +0800

    cc

---
+++ vue-newdemo/src/views/detail.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="top-blank">
+        <div class="detail-item flex">
+
+            <img :src="info.hash" />
+            <div>{{info.title}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                info:{},
+                id:this.$route.params.id
+            }
+        },
+        mounted(){
+            this.getAjax();
+            console.log(this.$route.params.id)
+        },
+        methods:{
+            getAjax(){
+                this.$http.get('../../static/data.json').then(
+                    (response)=>{
+                        this.info = response.body.data[this.id]
+                    },(err)=>{
+                        console.log(err)
+                    }
+                )
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .detail-item{
+        background: #fff;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding:15px;
+    }
+</style>
